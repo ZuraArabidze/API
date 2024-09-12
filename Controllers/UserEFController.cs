9cb17dc3be0dea622027f9c900df3653d98cbeb5
ZuraArabidze <z.arabidzee@gmail.com>
2024-09-12 11:18:03 +0400

    Create endpoints using Entity Framework

---
+++ Controllers/UserEFController.cs
@@ -0,0 +1,129 @@
+﻿using API.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using API.Models;
+using API.Dtos;
+using AutoMapper;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserEFController : ControllerBase
+    {
+        DataContextEF _entityFramework;
+        IMapper _mapper;
+
+        public UserEFController(IConfiguration config)
+        {
+            _entityFramework = new DataContextEF(config);
+            _mapper = new Mapper(new MapperConfiguration(cfg =>
+            {
+                // source,destination
+                cfg.CreateMap<UserToAddDto, User>();
+            }));
+        }
+
+        /// <summary>
+        /// Retrieves a list of users from the database.
+        /// </summary>
+        [HttpGet("GetUsers")]
+        public IEnumerable<User> GetUsers()
+        {
+            IEnumerable<User> users = _entityFramework.Users.ToList<User>();
+            return users;
+        }
+
+        /// <summary>
+        /// Retrieves a single user from the database based on the provided user ID.
+        /// </summary>
+        [HttpGet("GetSingleUser/{userId}")]
+        public User GetUser(int userId)
+        {
+            User? user = _entityFramework.Users.Where(u => u.UserId == userId).FirstOrDefault<User>();
+            if (user != null)
+            {
+                return user;
+            }
+            else
+            {
+                throw new Exception("Failed to Get User");
+            }
+            
+        }
+
+        /// <summary>
+        /// Updates the details of an existing user based on the provided user information.
+        /// </summary>
+        [HttpPut("EditUSer")]
+        public IActionResult EditUser(User user)
+        {
+            User? userDb = _entityFramework.Users.Where(u => u.UserId == user.UserId)
+                .FirstOrDefault<User>();
+
+            if (userDb != null)
+            {
+                userDb.FirstName = user.FirstName;
+                userDb.LastName = user.LastName;
+                userDb.Email = user.Email;
+                userDb.Gender = user.Gender;
+                userDb.Active = user.Active;
+
+                if (_entityFramework.SaveChanges() > 0)
+                {
+                    return Ok();
+                }
+                else
+                {
+                    throw new Exception("Failed to Update User");
+                }
+            }
+            else
+            {
+                throw new Exception("Failed to Get User");
+            }
+            
+        }
+
+        /// <summary>
+        /// Adds a new user to the database with the provided details.
+        /// </summary>
+        [HttpPost("AddUser")]
+        public IActionResult AddUser(UserToAddDto user)
+        {
+            User userDb = _mapper.Map<User>(user);
+
+            _entityFramework.Add(userDb);
+            if (_entityFramework.SaveChanges() > 0)
+            {
+                return Ok();
+            }
+
+            throw new Exception("Failed to Add User");
+        }
+
+        /// <summary>
+        /// Deletes a user from the database based on the provided user ID.
+        /// </summary>
+        [HttpDelete("DeleteUser/{userId}")]
+        public IActionResult DeleteUser(int userId)
+        {
+            User? userDb = _entityFramework.Users
+                .Where(u => u.UserId == userId)
+                .FirstOrDefault<User>();
+
+            if (userDb != null)
+            {
+                _entityFramework.Users.Remove(userDb);
+                if (_entityFramework.SaveChanges() > 0)
+                {
+                    return Ok();
+                }
+
+                throw new Exception("Failed to Delete User");
+            }
+
+            throw new Exception("Failed to Get User");
+        }
+    }
+}
