c5d716b0d4d6e26548068a196d82e36127836636
ZuraArabidze <z.arabidzee@gmail.com>
2024-09-12 10:44:51 +0400

    Add Entity Framework packages and configure DataContextEF class

---
+++ Data/DataContextEF.cs
@@ -0,0 +1,42 @@
+﻿using API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class DataContextEF : DbContext
+    {
+        private readonly IConfiguration _config;
+        public DataContextEF(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public virtual DbSet<User> Users { get; set; }
+        public virtual DbSet<UserJobInfo> UserJobInfo { get; set; }
+        public virtual DbSet<UserSalary> UserSalary { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(_config.GetConnectionString("DefaultConnection"),
+                    optionsBuilder => optionsBuilder.EnableRetryOnFailure());
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasDefaultSchema("dbo");
+            modelBuilder.Entity<User>()
+                .ToTable("Users", "dbo")
+                .HasKey(u => u.UserId);
+                //.HasNoKey()
+
+            modelBuilder.Entity<UserJobInfo>()
+                .HasKey(u => u.UserId);
+
+            modelBuilder.Entity<UserSalary>()
+                .HasKey(u => u.UserId);
+        }
+    }
+}
