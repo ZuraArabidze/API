1d9b695a2ba710d270b7f1f955b027ec306c92b1
ZuraArabidze <z.arabidzee@gmail.com>
2024-09-15 14:47:45 +0400

    Auth Settings and Models

---
+++ Dtos/UserForLoginConfirmationDto.cs
@@ -0,0 +1,9 @@
+﻿namespace API.Dtos
+{
+    public partial class UserForLoginConfirmationDto
+    {
+        byte[] PasswordHash { get; set; } = new byte[0];
+        byte[] PasswordSalt { get; set; } = new byte[0];
+
+    }
+}
---
+++ Dtos/UserForLoginDto.cs
@@ -0,0 +1,8 @@
+﻿namespace API.Dtos
+{
+    public partial class UserForLoginDto
+    {
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
