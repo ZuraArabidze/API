9698941b0b58d8459edb813aca2b3415c5ca271b
ZuraArabidze <z.arabidzee@gmail.com>
2024-09-17 21:54:24 +0400

    Exporting helper methods to a separate file

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,16 +1,11 @@
 ﻿using System.Data;
-using System.IdentityModel.Tokens.Jwt;
-using System.Security.Claims;
 using System.Security.Cryptography;
-using System.Text;
 using API.Data;
 using API.Dtos;
+using API.Helpers;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Cryptography.KeyDerivation;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Data.SqlClient;
-using Microsoft.IdentityModel.Tokens;
 
 namespace API.Controllers
 {
@@ -20,11 +15,11 @@ namespace API.Controllers
     public class AuthController : ControllerBase
     {
         private readonly DataContextDapper _dapper;
-        private readonly IConfiguration _config;
+        private readonly AuthHelper _authHelper;
         public AuthController(IConfiguration config)
         {
             _dapper = new DataContextDapper(config);
-            _config = config;
+            _authHelper = new AuthHelper(config);
         }
 
         [AllowAnonymous]
@@ -44,7 +39,7 @@ namespace API.Controllers
                         rng.GetNonZeroBytes(passwordSalt);
                     }
 
-                    byte[] passwordHash = GetPasswordHash(userForRegistration.Password, passwordSalt);
+                    byte[] passwordHash = _authHelper.GetPasswordHash(userForRegistration.Password, passwordSalt);
 
                     string sqlAddAuth = $"INSERT INTO Auth(Email,PasswordHash,PasswordSalt) " +
                                         $"VALUES('{userForRegistration.Email}', @PasswordHash, @PasswordSalt)";
@@ -94,7 +89,7 @@ namespace API.Controllers
             UserForLoginConfirmationDto userForConfirmation =
                 _dapper.LoadDataSingle<UserForLoginConfirmationDto>(sqlForHashAndSalt);
 
-            byte[] passwordHash = GetPasswordHash(userFoLogin.Password, userForConfirmation.PasswordSalt);
+            byte[] passwordHash = _authHelper.GetPasswordHash(userFoLogin.Password, userForConfirmation.PasswordSalt);
 
             //if(passwordHash == userForConfirmation.PasswordHash) Won't work because they are objects
             for (int index = 0; index < passwordHash.Length; index++)
@@ -110,7 +105,7 @@ namespace API.Controllers
 
             return Ok(new Dictionary<string,string>
             {
-                {"token", CreateToken(userId)}
+                {"token", _authHelper.CreateToken(userId)}
             });
         }
 
@@ -125,76 +120,8 @@ namespace API.Controllers
 
             return Ok(new Dictionary<string, string>
             {
-                {"token", CreateToken(userIdFromDb)}
+                {"token", _authHelper.CreateToken(userIdFromDb)}
             });
-        }
-
-        private byte[] GetPasswordHash(string password, byte[] passwordSalt)
-        {
-            string passwordSaltPlusString = _config.GetSection("AppSettings:PasswordKey").Value
-                                            + Convert.ToBase64String(passwordSalt);
-
-            byte[] passwordHash = KeyDerivation.Pbkdf2(
-                password: password,
-                salt: Encoding.ASCII.GetBytes(passwordSaltPlusString),
-                prf: KeyDerivationPrf.HMACSHA256,
-                iterationCount: 1000,
-                numBytesRequested: 256 / 8
-            );
-
-            return passwordHash;
-        }
-
-        // This method creates a JWT (JSON Web Token) for a user based on their userId.
-        private string CreateToken(int userId)
-        {
-            // 1. Define the claims (payload) of the JWT.
-            // Claims are the information that will be encoded into the token. 
-            // In this case, we're adding the user's ID as a claim.
-
-            Claim[] claims = new Claim[]
-            {
-                new Claim("userId", userId.ToString())
-            };
-
-            // 2. Create a security key for signing the token.
-            // The token key is retrieved from the configuration (Appsettings.json), and it's used
-            // to securely sign the token. SymmetricSecurityKey uses a shared secret for signing
-
-            SymmetricSecurityKey tokenKey = new SymmetricSecurityKey(
-                Encoding.UTF8.GetBytes(_config.GetSection("Appsettings:TokenKey").Value)
-            );
-
-            // 3. Define the signing credentials.
-            // These credentials tell the JWT how to sign the token (in this case using HMAC-SHA512).
-
-            SigningCredentials credentials = new SigningCredentials(
-                tokenKey, SecurityAlgorithms.HmacSha512Signature);
-
-            // 4. Create the security token descriptor.
-            // This object contains all the token's settings, like the subject (claims),
-            // signing credentials, and the expiration date.
-
-            SecurityTokenDescriptor descriptor = new SecurityTokenDescriptor()
-            {
-                Subject = new ClaimsIdentity(claims),
-                SigningCredentials = credentials,
-                Expires = DateTime.Now.AddDays(1)
-            };
-
-            // 5. Create the token handler that will generate the token.
-
-            JwtSecurityTokenHandler tokenHandler = new JwtSecurityTokenHandler();
-
-            // 6. Create the token based on the descriptor.
-            // This step actually builds the JWT.
-
-            SecurityToken token = tokenHandler.CreateToken(descriptor);
-
-            // 7. Return the generated JWT as a string.
-            // WriteToken serializes the token to a string, which can then be returned to the client.
-
-            return tokenHandler.WriteToken(token);
         }
     }
 }
---
+++ Helpers/AuthHelper.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace API.Helpers
+{
+    public class AuthHelper
+    {
+        private readonly IConfiguration _config;
+
+        public AuthHelper (IConfiguration config)
+        {
+            _config = config;
+        }
+        public byte[] GetPasswordHash(string password, byte[] passwordSalt)
+        {
+            string passwordSaltPlusString = _config.GetSection("AppSettings:PasswordKey").Value
+                                            + Convert.ToBase64String(passwordSalt);
+
+            byte[] passwordHash = KeyDerivation.Pbkdf2(
+                password: password,
+                salt: Encoding.ASCII.GetBytes(passwordSaltPlusString),
+                prf: KeyDerivationPrf.HMACSHA256,
+                iterationCount: 1000,
+                numBytesRequested: 256 / 8
+            );
+
+            return passwordHash;
+        }
+
+        // This method creates a JWT (JSON Web Token) for a user based on their userId.
+        public string CreateToken(int userId)
+        {
+            // 1. Define the claims (payload) of the JWT.
+            // Claims are the information that will be encoded into the token. 
+            // In this case, we're adding the user's ID as a claim.
+
+            Claim[] claims = new Claim[]
+            {
+                new Claim("userId", userId.ToString())
+            };
+
+            // 2. Create a security key for signing the token.
+            // The token key is retrieved from the configuration (Appsettings.json), and it's used
+            // to securely sign the token. SymmetricSecurityKey uses a shared secret for signing
+
+            SymmetricSecurityKey tokenKey = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(_config.GetSection("Appsettings:TokenKey").Value)
+            );
+
+            // 3. Define the signing credentials.
+            // These credentials tell the JWT how to sign the token (in this case using HMAC-SHA512).
+
+            SigningCredentials credentials = new SigningCredentials(
+                tokenKey, SecurityAlgorithms.HmacSha512Signature);
+
+            // 4. Create the security token descriptor.
+            // This object contains all the token's settings, like the subject (claims),
+            // signing credentials, and the expiration date.
+
+            SecurityTokenDescriptor descriptor = new SecurityTokenDescriptor()
+            {
+                Subject = new ClaimsIdentity(claims),
+                SigningCredentials = credentials,
+                Expires = DateTime.Now.AddDays(1)
+            };
+
+            // 5. Create the token handler that will generate the token.
+
+            JwtSecurityTokenHandler tokenHandler = new JwtSecurityTokenHandler();
+
+            // 6. Create the token based on the descriptor.
+            // This step actually builds the JWT.
+
+            SecurityToken token = tokenHandler.CreateToken(descriptor);
+
+            // 7. Return the generated JWT as a string.
+            // WriteToken serializes the token to a string, which can then be returned to the client.
+
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
